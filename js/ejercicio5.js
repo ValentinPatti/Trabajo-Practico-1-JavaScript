5438c7bc05e94bc8b924b992b5563d8b8336a032
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-03-05 21:09:49 -0300

    Ejercicio 5 resuelto

---
+++ js/ejercicio5.js
@@ -0,0 +1,6 @@
+//Escribe un programa de tres líneas que pida un número, pida otro número 
+//y escriba el resultado de sumar estos dos números.
+
+const numero1 = Number(prompt('Ingresa el primer número'));
+const numero2 = Number(prompt('Ingresa el segundo número'));
+document.writeln(numero1 + numero2);
